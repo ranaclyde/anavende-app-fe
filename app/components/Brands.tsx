310a9caf605086aed50ec88b499e6d3565990209
Matías E Sanhueza <manoloesanhueza@gmail.com>
2025-06-30 21:55:11 -0300

    Update the brands component

--- app/components/Brands.tsx
+++ app/components/Brands.tsx
@@ -1,6 +1,50 @@
-/* eslint-disable @next/next/no-img-element */
 import React from 'react'
+import Image from 'next/image'
+
 import Container from './ui/Container'
+
+const brands = [
+  {
+    name: 'Suono',
+    image: '/images/brands/suono.jpg',
+    alt: 'Suono logo',
+  },
+  {
+    name: 'GENIUS',
+    image: '/images/brands/GENIUS.png',
+    alt: 'GENIUS logo',
+  },
+  {
+    name: 'KINGSTON',
+    image: '/images/brands/KINGSTON.png',
+    alt: 'KINGSTON logo',
+  },
+  {
+    name: 'Marvo',
+    image: '/images/brands/marvo.png',
+    alt: 'Marvo logo',
+  },
+  {
+    name: 'NETMAK',
+    image: '/images/brands/NETMAK.png',
+    alt: 'NETMAK logo',
+  },
+  {
+    name: 'Redragon',
+    image: '/images/brands/redragon.png',
+    alt: 'Redragon logo',
+  },
+  {
+    name: 'SANDISK',
+    image: '/images/brands/SANDISK.png',
+    alt: 'SANDISK logo',
+  },
+  {
+    name: 'X-Trike Me',
+    image: '/images/brands/x-trike-me.jpg',
+    alt: 'X-Trike Me logo',
+  },
+]
 
 const Brands = () => {
   return (
@@ -8,73 +52,32 @@ const Brands = () => {
       <Container className="flex flex-wrap py-20">
         <div className="w-full max-w-full flex-shrink-0 lg:mt-2 lg:w-1/3 lg:flex-none">
           <h2 className="mb-3 text-xs font-bold uppercase tracking-wide text-gray-500 xl:text-base">
-            Our Clients
+            Nuestras marcas
           </h2>
           <h3 className="mb-3 font-bold text-gray-800 sm:text-2xl xl:text-4xl">
-            Trusted by over 300+ clients
+            Algunas marcas con las que trabajamos
           </h3>
-          <p className="">
-            We bring solutions to make life easier for our customers.
+          <p>
+            Conoce algunas de las marcas con las que trabajamos para ofrecerte
+            la mejor relacion precio/calidad.
           </p>
         </div>
         <div className="w-full max-w-full py-10 lg:w-2/3 lg:flex-none lg:px-8 lg:py-0">
-          <div className="grid grid-cols-2 gap-4 sm:grid-cols-4">
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/znFUHjtfFGgWw3iwf6L7U.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/1jqHy4mfbbJdowYMMYmv4.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/fYCAxh-v8cM9_Kz5F_NWc.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/qr76A1CV-Bngcu7a43OtD.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/tNngfZmftaEAbhB6-nkdB.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/_zdDNHsf3wwrPIPNx_5YL.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/0theMmZM4a-L5PWRswKPD.png"
-                alt=""
-                className=""
-              />
-            </div>
-            <div className="w-32">
-              <img
-                src="https://componentland.com/images/famjKUew9dTPjjl1stpS7.png"
-                alt=""
-                className=""
-              />
-            </div>
+          <div className="grid grid-cols-2 gap-5 sm:grid-cols-4">
+            {brands.map((brand) => (
+              <div
+                key={brand.name}
+                className="w-32 h-[85px] flex items-center justify-center justify-self-center"
+              >
+                <Image
+                  src={brand.image}
+                  alt={brand.alt}
+                  width={128}
+                  height={85}
+                  className="object-cover"
+                />
+              </div>
+            ))}
           </div>
         </div>
       </Container>
